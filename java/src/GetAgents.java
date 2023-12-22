2e9c1e1b753bfe5a88f9b506baf22ce8d0a4cd6c
stevdef <72949019+stevdef@users.noreply.github.com>
2023-12-22 12:07:05 +0100

    Update GetAgents.java

--- java/src/GetAgents.java
+++ java/src/GetAgents.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-@SuppressWarnings({ "unused"})
+@SuppressWarnings({ "unused" })
 public class GetAgents {
-	
+
 	private static String format = "|%1$-6s|%2$-26s|%3$-10s|%4$-6s|%5$-6s|%6$-12s|%7$-10s|%8$-17s|%9$-6s|%10$-10s|\n";
-	
+
 	public GetAgents() {
-		
+
 	}
-	
+
 	public static ArrayList<Agent> getAgentList() throws IOException, InterruptedException {
 		ArrayList<Agent> agentList = new ArrayList<Agent>();
 		TelnetConnect tc = new TelnetConnect();
 		tc.write("tabag");
 		String output = tc.readUntil("(110");
 		String[] lines = output.split(System.getProperty("line.separator"));
-		
-		for(int i = 0; i < lines.length; i++) {
+
+		for (int i = 0; i < lines.length; i++) {
 			if (lines[i].contains("|     ")) {
 				boolean loggedOn = false;
 				String agentStation = "";
@@ -27,60 +27,52 @@ public class GetAgents {
 				String agentState = "Logged Off";
 				String agentWithdrawalType = "";
 				String agentDynamicState = "";
-				String agentExtension = lines[i].substring(26,30);
+				String agentExtension = lines[i].substring(26, 30);
 				boolean office = false;
 				String agentIpAddress = "0.0.0.0";
 				System.out.println("AgentExtension = " + agentExtension);
-				String agentFullName = getAgentFullName(lines[i].substring(48,64).trim());
+				String agentFullName = getAgentFullName(lines[i].substring(48, 64).trim());
 				System.out.println("AgentFullName = " + agentFullName);
-				String proAcd = lines[i].substring(82,86).trim();
+				String proAcd = lines[i].substring(75, 80).trim();
 				System.out.println("proAcd = " + proAcd);
-				if(proAcd.length() > 0) {
+				if (proAcd.length() > 0) {
 					loggedOn = true;
 					agentStation = proAcd;
 					agentPG = getAgentPG(agentExtension, tc);
 					System.out.println("AgentPG = " + agentPG);
 					agentState = getAgentState(agentExtension, tc);
 					System.out.println("AgentState = " + agentState);
-					if(agentState.equals("Unavailable")) {
+					if (agentState.equals("Unavailable")) {
 						agentWithdrawalType = getAgentWithdrawalType(agentExtension, tc);
 					}
 					agentDynamicState = getAgentDynamicState(agentExtension, tc);
 					System.out.println("AgentDynamicState = " + agentDynamicState);
-					if(!agentDynamicState.equals("Out of service")) {
+					if (!agentDynamicState.equals("Out of service")) {
 						agentIpAddress = getAgentIpAddress(agentExtension, tc);
 					}
-					System.out.println(agentIpAddress);
-					
-					if(!agentIpAddress.equals("0.0.0.0")) {
-						if(agentIpAddress.contains("10.100.201") || agentIpAddress.contains("10.28.34") || agentIpAddress.contains("10.28.101")) {
-							if(agentStation.equals("4867")) {
-								office = false;
-							} else {
-								office = true;
-							}
-						} else {
-							office = false;
+					System.out.println("AgentIpAddress = " + agentIpAddress);
+
+					if (!agentIpAddress.equals("0.0.0.0")) {
+						if (agentIpAddress.contains("10.100.201") || agentIpAddress.contains("10.28.34")
+								|| agentIpAddress.contains("10.28.101")) {
+							office = true;
 						}
 					}
-					System.out.println(office);
-					
-					// if(agentIpAddress.substring(beginIndex))
-					
 				}
-				if(!agentExtension.equals("4003") && !agentExtension.equals("4008")) {
-					
-					agentList.add(new Agent(agentFullName, agentExtension, agentStation, 
-							agentPG, loggedOn, agentState, agentWithdrawalType, agentDynamicState, office, agentIpAddress));
-				}				
+				if (!agentExtension.equals("4003") && !agentExtension.equals("4008")) {
+
+					agentList.add(new Agent(agentFullName, agentExtension, agentStation, agentPG, loggedOn, agentState,
+							agentWithdrawalType, agentDynamicState, office, agentIpAddress));
+				}
 			}
 		}
 		ArrayList<Agent> voiceAgentList = new ArrayList<Agent>();
 		ArrayList<Agent> dataAgentList = new ArrayList<Agent>();
-		for(Agent agent : agentList) {
+		for (Agent agent : agentList) {
 			String agentName = agent.getAgentName();
-			if(agentName.equals("Steve Dams") || agentName.equals("Steven Deferme") || agentName.equals("Anthony Holderbeke") 
-					|| agentName.equals("Adrian Stancu") || agentName.equals("Tim Willems")) {
+			if (agentName.equals("Steve Dams") || agentName.equals("Steven Deferme")
+					|| agentName.equals("Anthony Holderbeke") || agentName.equals("Adrian Stancu")
+					|| agentName.equals("Tim Willems") || agentName.equals("Nicolas Dricot")) {
 				voiceAgentList.add(agent);
 			} else {
 				dataAgentList.add(agent);
@@ -88,133 +80,136 @@ public class GetAgents {
 		}
 		Collections.sort(voiceAgentList);
 		Collections.sort(dataAgentList);
-		
+
 		ArrayList<Agent> sortedAgentList = new ArrayList<Agent>(voiceAgentList);
 		sortedAgentList.addAll(dataAgentList);
 		tc.disconnect();
 		return sortedAgentList;
 	}
-	
+
 	private static String getAgentPG(String agentExtension, TelnetConnect tc) {
 		String agentPG = "";
 		String pgNumber = "";
 		tc.write("agacd " + agentExtension);
 		String output = tc.readUntil("(110");
 		String[] lines = output.split(System.getProperty("line.separator"));
-			
-		for(int i = 0; i < lines.length; i++) {
+
+		for (int i = 0; i < lines.length; i++) {
 			if (lines[i].contains("Affect PG Dir nb")) {
-				pgNumber = lines[i].substring(32,36).trim(); 
+				pgNumber = lines[i].substring(32, 36).trim();
 			}
 		}
-		if(pgNumber.length() > 0) {
+		if (pgNumber.length() > 0) {
 			tc.write("pgwqlist " + pgNumber);
 			String output2 = tc.readUntil("(110");
 			String[] lines2 = output2.split(System.getProperty("line.separator"));
-			for(int i = 0; i < lines2.length; i++) {
+			for (int i = 0; i < lines2.length; i++) {
 				if (lines2[i].contains("Name")) {
-					String agentPGString = lines2[i].substring(20,37).trim();
-					if(agentPGString.equals("VOICE")) {
+					String agentPGString = lines2[i].substring(20, 37).trim();
+					if (agentPGString.equals("VOICE")) {
 						agentPG = "Voice";
-					} else if(agentPGString.equals("DATA")) {
+					} else if (agentPGString.equals("DATA")) {
 						agentPG = "Data";
 					} else if (agentPGString.equals("AXA")) {
 						agentPG = "Axa";
 					}
-				} 
+				}
 			}
 		}
 		return agentPG;
 	}
-	
+
 	private static String getAgentState(String agentExtension, TelnetConnect tc) {
 		String agentState = "";
 		tc.write("agacd " + agentExtension);
 		String output = tc.readUntil("(110");
 		String[] lines2 = output.split(System.getProperty("line.separator"));
-		for(int i = 0; i < lines2.length; i++) {
+		for (int i = 0; i < lines2.length; i++) {
+
 			if (lines2[i].contains("Static State")) {
 				String state = lines2[i].substring(25, 36).trim();
-				if(state.equals("NORMAL")) {
+				if (state.equals("NORMAL")) {
 					agentState = "Logged On";
-				} else if(state.equals("WITH_DRAWAL")) {
+				} else if (state.equals("WITH_DRAWAL")) {
 					agentState = "Unavailable";
-				} else if(state.equals("PRE_AFF")) {
+				} else if (state.equals("PRE_AFF")) {
 					agentState = "Logged On";
 				}
 			}
 		}
 		return agentState;
 	}
-	
+
 	private static String getAgentWithdrawalType(String agentExtension, TelnetConnect tc) {
-		String agentWithdrawalType ="";
+		String agentWithdrawalType = "";
 		tc.write("agacd " + agentExtension);
 		String output = tc.readUntil("(110");
 		String[] lines2 = output.split(System.getProperty("line.separator"));
-		for(int i = 0; i < lines2.length; i++) {
+		for (int i = 0; i < lines2.length; i++) {
+
 			if (lines2[i].contains("Withd type")) {
-				String withdrawalType = lines2[i].substring(35, 36); 
-				if(withdrawalType.equals("1")) {
+				String withdrawalType = lines2[i].substring(35, 36);
+				if (withdrawalType.equals("1")) {
 					agentWithdrawalType = "Meeting";
-				} else if(withdrawalType.equals("2")) {
+				} else if (withdrawalType.equals("2")) {
 					agentWithdrawalType = "Lunch";
-				} else if(withdrawalType.equals("3")) {
+				} else if (withdrawalType.equals("3")) {
 					agentWithdrawalType = "Intervention";
-				} else if(withdrawalType.equals("4")) {
+				} else if (withdrawalType.equals("4")) {
 					agentWithdrawalType = "Labo";
-				} else if(withdrawalType.equals("5")) {
+				} else if (withdrawalType.equals("5")) {
 					agentWithdrawalType = "Break";
 				}
 			}
 		}
 		return agentWithdrawalType;
 	}
-	
+
 	private static String getAgentDynamicState(String agentExtension, TelnetConnect tc) {
 		String agentDynamicState = "";
 		tc.write("agacd " + agentExtension);
 		String output = tc.readUntil("(110");
 		String[] lines2 = output.split(System.getProperty("line.separator"));
-		for(int i = 0; i < lines2.length; i++) {
+		for (int i = 0; i < lines2.length; i++) {
+
 			if (lines2[i].contains("Dynamic State")) {
-				String dynamicState = lines2[i].substring(58,71).trim();
-				if(dynamicState.equals("OUT OF SERV")) {
+				String dynamicState = lines2[i].substring(58, 71).trim();
+				if (dynamicState.equals("OUT OF SERV")) {
 					agentDynamicState = "Out of service";
-				} else if(dynamicState.equals("FREE")) {
+				} else if (dynamicState.equals("FREE")) {
 					agentDynamicState = "Free";
-				} else if(dynamicState.contains("RING")) {
+				} else if (dynamicState.contains("RING")) {
 					agentDynamicState = "Ringing";
-				} else if(dynamicState.equals("LOC ACD CONV") || dynamicState.equals("EXT ACD CONV")) {
+				} else if (dynamicState.equals("LOC ACD CONV") || dynamicState.equals("EXT ACD CONV")) {
 					agentDynamicState = "In conversation";
-				} else if(dynamicState.equals("LOC PRIV CONV") || dynamicState.equals("EXT PRIV CONV")) {
+				} else if (dynamicState.equals("LOC PRIV CONV") || dynamicState.equals("EXT PRIV CONV")) {
 					agentDynamicState = "In conversation";
-				} else if(dynamicState.equals("FIN PAUSE")) {
+				} else if (dynamicState.equals("FIN PAUSE")) {
 					agentDynamicState = "No PG";
-				} else if(dynamicState.equals("IDLE WRAP UP")) {
+				} else if (dynamicState.equals("IDLE WRAP UP")) {
 					agentDynamicState = "Wrap up";
 				}
 			}
 		}
 		return agentDynamicState;
 	}
-		
+
 	private static String getAgentIpAddress(String agentExtension, TelnetConnect tc) {
 		String ipAddress = "0.0.0.0";
 		tc.write("ippstat d " + agentExtension);
 		String output = tc.readUntil("(110");
 		String[] lines = output.split(System.getProperty("line.separator"));
-		for(int i = 0; i < lines.length; i++) {
+		for (int i = 0; i < lines.length; i++) {
 			if (lines[i].contains("IPv4")) {
-				ipAddress = lines[i].substring(71,87).trim(); 
+				ipAddress = lines[i].substring(71, 87).trim();
 			}
 		}
 		return ipAddress;
 	}
-	
+
 	public static String getAgentFullName(String agentName) {
 		String agentFullName = "";
-		switch(agentName) {
+		switch (agentName) {
 		case "WILLEMS AGEN Tim":
 			agentFullName = "Tim Willems";
 			break;
@@ -248,24 +243,30 @@ public class GetAgents {
 		case "SCHIETTECAT  Chr":
 			agentFullName = "Christophe Schiettecat";
 			break;
+		case "DRICOT AGENT Nic":
+			agentFullName = "Nicolas Dricot";
+			break;
 		default:
 			agentFullName = agentName;
 			break;
 		}
-		
+
 		return agentFullName;
 	}
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		try {
 			ArrayList<Agent> agentList = new ArrayList<Agent>();
 			agentList = getAgentList();
-			for(Agent agent : agentList) {
-				System.out.format(format, agent.getAgentExtension(), agent.getAgentName() , String.valueOf(agent.getLoggedOn()), 
-						agent.getAgentStation(), agent.getAgentPG(), agent.getAgentState(), 
-						agent.getAgentWithdrawalType(), agent.getAgentDynamicState(), agent.getOffice(), agent.getIpAddress());
-				
-			}
+			/*
+			 * for(Agent agent : agentList) { System.out.format(format,
+			 * agent.getAgentExtension(), agent.getAgentName() ,
+			 * String.valueOf(agent.getLoggedOn()), agent.getAgentStation(),
+			 * agent.getAgentPG(), agent.getAgentState(), agent.getAgentWithdrawalType(),
+			 * agent.getAgentDynamicState(), agent.getOffice(), agent.getIpAddress());
+			 * 
+			 * }
+			 */
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
